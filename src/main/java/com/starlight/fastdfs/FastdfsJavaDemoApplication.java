64d4ade34ceb58230067c4750e1d0f573839aeae
hxguang <hxguang911219@163.com>
2018-11-14 09:44:21 +0800

    Initial commit

---
+++ src/main/java/com/starlight/fastdfs/FastdfsJavaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.starlight.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdfsJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastdfsJavaDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/starlight/fastdfs/FastdfsJavaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.starlight.fastdfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastdfsJavaDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
